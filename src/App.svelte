b0a47addd4931516b61ca169df8f28ce0ccd7254
George Tudor <Alexander.George.Tudor@gmail.com>
2024-01-27 21:54:15 -0600

    Project Setup and basic movie component created

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+import Header from "./lib/Header.svelte";
+import VideoItem from "./lib/VideoItem.svelte";
+
+const blurays = [
+    {
+        id: 'b1',
+        title: 'The Avengers',
+        subtitle: 'Some assembly required.',
+        cast: 'Robert Downey Jr, Chris Evans, Scarlett Johansson',
+        imageUrl: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg',
+        description: 'When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!'
+    },
+    {
+        id: 'b2',
+        title: 'Batman',
+        subtitle: 'Justice is always darkest before the dawn.',
+        cast: 'Michael Keaton, Jack Nicholson, Kim Basinger'        ,
+        imageUrl: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/cij4dd21v2Rk2YtUQbV5kW69WB2.jpg',
+        description: 'Batman must face his most ruthless nemesis when a deformed madman calling himself "The Joker" seizes control of Gotham\'s criminal underworld.'
+    }
+]
+</script>
+
+<style>
+    #blurays {
+        margin-top: 5rem;
+    }
+</style>
+
+<Header />
+
+<section id="blurays">
+    {#each blurays as bluray}
+        <VideoItem
+                title={bluray.title}
+                subtitle={bluray.subtitle}
+                cast={bluray.cast}
+                imageUrl={bluray.imageUrl}
+                description={bluray.description.substring(0, 120) + '...'}/>
+    {/each}
+</section>
---
+++ src/lib/VideoItem.svelte
@@ -0,0 +1,84 @@
+<script>
+    export let title;
+    export let subtitle;
+    export let cast;
+    export let imageUrl;
+    export let description;
+
+</script>
+
+<style>
+    article {
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        border-radius: 5px;
+        background: #f9fbfc;
+        margin: 1rem;
+    }
+
+    header,
+    .content,
+    footer {
+        padding: 1rem;
+        color: #1a1a1a;
+    }
+
+    .image {
+        width: 100%;
+        height: 28rem;
+    }
+
+    .image img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 65%;
+    }
+
+    h1 {
+        font-size: 1.5rem;
+        margin: 0.5rem 0;
+        font-family: "Roboto Slab", sans-serif;
+        color: #1a1a1a;
+    }
+
+
+    h2 {
+        font-size: 1rem;
+        color: #808080;
+        margin: 0.5rem 0;
+    }
+
+    h3 {
+        font-size: 1rem;
+        margin: 0.5rem 0;
+        font-family: "Roboto Slab", sans-serif;
+        color: #1a1a1a;
+    }
+
+    p {
+        font-size: 1.25rem;
+        margin: 0;
+    }
+
+    div {
+        text-align: right;
+    }
+</style>
+
+<article>
+    <header>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <h3>{cast}</h3>
+    </header>
+    <div class ="image">
+        <img src={imageUrl} alt={title}>
+    </div>
+    <div class="content">
+        <p>{description}</p>
+    </div>
+    <footer>
+        <button>Add to Cart</button>
+        <button>Show Details</button>
+    </footer>
+</article>
